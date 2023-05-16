7f8f827b7287b698ed70e3912dbae67e41208864
LoganDobson <112992719+LoganDobson@users.noreply.github.com>
2023-05-16 19:41:14 -0400

    polished

--- src/main/java/com/example/comp1011week2/BookViewController.java
+++ src/main/java/com/example/comp1011week2/BookViewController.java
@@ -1,13 +1,11 @@
 package com.example.comp1011week2;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -40,10 +38,10 @@ public class BookViewController implements Initializable {
             String bookName = bookNameTextField.getText();
             String author = authorTextField.getText();
             String genre = genreComboBox.getSelectionModel().getSelectedItem();
-            //double price = priceSpinner.getValue();
+            double price = priceSpinner.getValue();
             boolean available = availabilityCheckBox.isSelected();
 
-            Book book1 = new Book(1, bookName, author, genre, 12.50, available);
+            Book book1 = new Book(1, bookName, author, genre, price, available);
             finalLabel.setVisible(true);
             finalLabel.setText(book1.toString());
         }
@@ -57,7 +55,25 @@ public class BookViewController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         finalLabel.setVisible(false);
         genreComboBox.getItems().addAll(Book.findGenres());
+        SpinnerValueFactory<Double> spinnerValueFactory = new SpinnerValueFactory.DoubleSpinnerValueFactory(20.00,200.00,2.00,5.00);
+        priceSpinner.setValueFactory(spinnerValueFactory);
+        priceSpinner.setEditable(true);
 
+        // anonymous inner class
+
+        TextField spinnerTextField =  priceSpinner.getEditor();
+
+        spinnerTextField.textProperty().addListener((observableValue, oldValue, newValue) -> {
+            try{
+                Double.parseDouble(newValue);
+            }catch (Exception e){
+                finalLabel.setVisible(true);
+                finalLabel.setText("only double values are allowed for price");
+                e.printStackTrace();
+                spinnerTextField.setText(oldValue);
+
+            }
+        });
     }
 
 
